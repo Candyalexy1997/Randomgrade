9eba866e7751c5c26cbefcc56138e25964fdb5c8
Candice Whiskey <candicewhiskey77@gmail.com>
2021-03-21 01:19:58 -0400

    minor updates for readability

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,7 +16,6 @@ alpha = ["a", "b","c","d", "e", "f","g","h","i","j","k","l","m","n","o","p","q",
 //space
 space =[];
 
-// Choices declared outside the if statement so they can be concatenated upon condition
 var choices;
 // converts letters to uppercase 
 var toUpper = function (x) {
@@ -57,18 +56,18 @@ function generatePassword() {
       confirmLowercase = confirm("Will this contain Lowercase letters?");
   };
 
-  // Else if for 4 negative options
+  //  4 negative options alert shown
   if (!confirmCharacter && !confirmNumber && !confirmUppercase && !confirmLowercase) {
       choices = alert("You must choose a criteria!");
 
   }
-  // First if statement that uses user input prompts to determine choices
-  // Else if for 4 positive options
+  // tatement that uses user input prompts to determine choices
+
   else if (confirmCharacter && confirmNumber && confirmUppercase && confirmLowercase) {
 
       choices = character.concat(number, alpha, alpha2);
   }
-  // Else if for 3 positive options
+ 
   else if (confirmCharacter && confirmNumber && confirmUppercase) {
       choices = character.concat(number, alpha2);
   }
@@ -81,26 +80,7 @@ function generatePassword() {
   else if (confirmNumber && confirmLowercase && confirmUppercase) {
       choices = number.concat(alpha, alpha2);
   }
-   // Else if for 2 positive options 
-   else if (confirmCharacter && confirmNumber) {
-    choices = character.concat(number);
-
-} else if (confirmCharacter && confirmLowercase) {
-    choices = character.concat(alpha);
-
-} else if (confirmCharacter && confirmUppercase) {
-    choices = character.concat(alpha2);
-}
-else if (confirmLowercase && confirmNumber) {
-    choices = alpha.concat(number);
-
-} else if (confirmLowercase && confirmUppercase) {
-    choices = alpha.concat(alpha2);
-
-} else if (confirmNumber && confirmUppercase) {
-    choices = number.concat(alpha2);
-}
-// Else if for 1 positive option
+ 
 else if (confirmCharacter) {
     choices = character;
 }
